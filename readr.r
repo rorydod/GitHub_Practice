5db0e1925bb189b7e6552ab399ae629a40bc9425
Rory O'Donnell <rorydod@umich.edu>
2016-06-02 13:12:48 +0200

    second commit(2)

--- readr.r
+++ readr.r
@@ -4,3 +4,9 @@ class(illness$age)
 illness$age <- as.numeric(illness$age)
 class(illness$age)
 class(illness$sex)
+illness$sex <- as.character(illness$sex)
+female <- which(illness$sex == "2")
+illness$sex[female] <- 'Female'
+male <- which(illness$sex == "1")
+illness$sex[male] <- 'Male'
+illness$sex
